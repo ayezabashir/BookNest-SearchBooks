dbe2333812d471c474d5f20af68ab65e5bdbc43f
Ayeza <ayezabashir442@gmail.com>
2023-08-30 00:23:59 +0500

    search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,16 +1,14 @@
 import { BiSearchAlt } from 'react-icons/bi'
 const Search = () => {
     return (
-        <div className="search">
-            <div className="search-container">
-                <div className="search-content">
-                    <form className="search-form">
-                        <input type="text" placeholder='Search any book...' />
-                        <button type="submit">
-                            <BiSearchAlt className='icon' />
-                        </button>
-                    </form>
-                </div>
+        <div className="search-container">
+            <div className="search-content">
+                <form className="search-form">
+                    <input type="text" placeholder='Search any book...' />
+                    <button type="submit">
+                        <BiSearchAlt className='icon' />
+                    </button>
+                </form>
             </div>
         </div>
     )
