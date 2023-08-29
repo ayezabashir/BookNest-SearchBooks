a1cee5380001fd4499bf11bde97306e6ac07c8d3
Ayeza <ayezabashir442@gmail.com>
2023-08-30 00:48:11 +0500

    getting data from api

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { BiSearchAlt } from 'react-icons/bi'
+const api_url = 'https://openlibrary.org/search.json?title=';
+
 const Search = () => {
+    const [inputtext, setInputText] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    }
+
+    const fetchBookData = (event) => {
+        event.preventDefault();
+        let searchWord = api_url + inputtext;
+        console.log(searchWord);
+    }
+
     return (
         <div className="search-container">
             <div className="search-content">
                 <form className="search-form">
-                    <input type="text" placeholder='Search any book...' />
-                    <button type="submit">
+                    <input
+                        type="text"
+                        onChange={handleInputChange}
+                        value={inputtext}
+                        placeholder='Search any book...'
+                    />
+                    <button type="submit" onClick={fetchBookData}>
                         <BiSearchAlt className='icon' />
                     </button>
                 </form>
@@ -14,4 +34,4 @@ const Search = () => {
     )
 }
 
-export default Search
+export default Search;
