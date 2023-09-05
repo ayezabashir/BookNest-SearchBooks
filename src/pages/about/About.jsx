ed8d81a9aae1695d67fe443299009f50813ae0ad
Ayeza <ayezabashir442@gmail.com>
2023-09-06 00:32:37 +0500

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,21 @@
+import about from '/about.jpg'
 const About = () => {
     return (
-        <div>
-            About
+        <div className="about">
+            <div className="container">
+                <div className="about-content">
+                    <div className="about-img">
+                        <img src={about} alt="" />
+                    </div>
+                    <div className="about-text">
+                        <h2>Who are We?</h2>
+                        <p>Consectetur adipisicing elit. Voluptatum, vitae doloribus culpa corrupti quae ad necessitatibus eum vel quidem expedita quaerat non consequuntur, cumque minus est maiores laudantium magnam! Exercitationem tempore atque excepturi, delectus optio natus nostrum.</p>
+                        <h2>A message from Our Co-Founder</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deserunt earum, delectus tenetur accusamus ex. Quasi, perspiciatis sed? Unde explicabo commodi suscipit vel in dolores cupiditate exercitationem ipsam illum veritatis?</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
